672ab06ced8611ee44a34515415834d18511af80
thnguyet <nguyennguyet20052018@gmail.com>
2026-05-06 20:22:02 +0700

    delete data seeder

--- be/order-service/src/main/java/org/gomsu/orderservice/OrderServiceApplication.java
+++ be/order-service/src/main/java/org/gomsu/orderservice/OrderServiceApplication.java
@@ -23,38 +23,6 @@ public class OrderServiceApplication {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner initData(ShippingMethodRepository shipRepo, PaymentMethodRepository payRepo) {
-        return args -> {
-            if (shipRepo.count() == 0) {
-                System.out.println(">>> Đang khởi tạo đơn vị vận chuyển...");
-                ShippingMethod sn = new ShippingMethod();
-                sn.setName("Giao hàng Nhanh");
-                sn.setShippingFee(30000.0);
-                sn.setActive(true);
-                shipRepo.save(sn);
 
-                ShippingMethod st = new ShippingMethod();
-                st.setName("Giao hàng Tiết kiệm");
-                st.setShippingFee(15000.0);
-                st.setActive(true);
-                shipRepo.save(st);
-            }
-            if (payRepo.count() == 0) {
-                System.out.println(">>> Đang khởi tạo phương thức thanh toán...");
-                PaymentMethod p1 = new PaymentMethod();
-                p1.setName("Chuyển khoản ngân hàng");
-                p1.setPaymentCode("BANK_TRANSFER");
-                p1.setActive(true);
-                payRepo.save(p1);
-
-                PaymentMethod p2 = new PaymentMethod();
-                p2.setName("Thanh toán khi nhận hàng (COD)");
-                p2.setPaymentCode("COD");
-                p2.setActive(true);
-                payRepo.save(p2);
-            }
-        };
-    }
 
 }
